dd782b8b44057504e3c2ede7b7e82436105aaf88
Zwampyh <ich3n3y@gmail.com>
2022-05-14 19:14:45 -0600

    Awesomeness

--- Bullet.gd
+++ Bullet.gd
@@ -1,26 +1,53 @@
-extends Area2D
+extends KinematicBody2D
+onready var Sprite = $Sprite
 
-var speed = 100
-
+var speed = 200
+var velocity = Vector2.ZERO
 var asteroid_position = 1
-var asteroid_radius = 1
+var gun_distance = 1
+var his_facing = "right"
 
-
-func b_init(asteroid_p, asteroid_r):
+func b_init(asteroid_p, asteroid_r, facing):
 	asteroid_position = asteroid_p
-	asteroid_radius = asteroid_r
+	gun_distance = asteroid_r
+	his_facing = facing
+	if his_facing == "left":
+		$Sprite.flip_h = true
+	else:
+		pass
 
 func _ready():
 	pass
 
 
 func _physics_process(delta):
-	var amount_over = (asteroid_position - position).length()-asteroid_radius
-	if amount_over>0:
-		position = asteroid_position + -(asteroid_position - position).normalized()*asteroid_radius
+	var rightvec=(asteroid_position - position).rotated(-PI/2).normalized()
+	var leftvec=(asteroid_position - position).rotated(PI/2).normalized()
+	var newvelocity = rightvec
+	if his_facing == "right":
+		newvelocity = rightvec
+	else:
+		newvelocity = leftvec
+	velocity = newvelocity * speed
+	velocity = move_and_slide(velocity)
+	
+	
+	
+	
+	look_at(asteroid_position)
+	rotation-=PI/2
+	
+	var amount_over = (asteroid_position - position).length()-gun_distance
+	
+	position = asteroid_position + -(asteroid_position - position).normalized()*gun_distance
+	
+		
 
 
-func _on_Bullet_body_entered(body):
-	if body.is_in_group("enemy"):
+
+
+
+func _on_Area2D_area_entered(area):
+	if area.is_in_group("enemy_hitbox"):
 		print("bulletdead")
 		queue_free()
